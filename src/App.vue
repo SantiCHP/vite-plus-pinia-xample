6b8592d2b1152a09ea94e34b852ed0480dd21b82
SantiCHP <santichadmin@gmail.com>
2022-06-09 23:39:03 +0100

    2nd commit

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<script setup>
+import { useUsersStore } from './stores/users'
+import { ref } from 'vue'
+
+const user_store = useUsersStore()
+
+const user_input = ref({
+  name: '',
+  email: ''
+})
+
+const sort = ref( false )
+
+const CreateUser = () => {
+  if( 
+      !user_input.value.name || 
+      !user_input.value.email 
+  ) {
+      return alert( 'Please enter both name and email' )
+  }
+
+  user_store.create( user_input.value )
+
+  user_input.value = {
+    name: '',
+    email: ''
+  }
+}
+
+const DeleteUser = id => {
+  user_store.delete( id )
+  alert( `User deleted with id: ${ id }` )
+}
+</script>
+
+<template>
+
+  <main>
+    <h1>Team manager</h1>
+
+    <form @submit.prevent="CreateUser">
+      <input 
+        type="text"
+        placeholder="Your Name and Surname"
+        v-model="user_input.name" />
+      <input 
+        type="email"
+        placeholder="you@email.com"
+        v-model="user_input.email" />
+      <input 
+        type="submit" 
+        value="Create user" />
+    </form>
+
+    <label><span>Sort by user name</span><input type="checkbox" v-model="sort"></label>
+
+    <div class="users" v-if="!sort">
+      <div v-for="user in user_store.users" class="user">
+          <div>ID: {{ user.id }}</div>
+          <h3>{{ user.name }}</h3>
+          <p>{{  user.email }}</p>
+          <button @click="DeleteUser(user.id)">Delete</button>
+      </div>
+    </div>
+
+    <div class="users" v-else>
+      <div v-for="user in user_store.usersByName" class="user">
+          <div>ID: {{ user.id }}</div>
+          <h3>{{ user.name }}</h3>
+          <p>{{  user.email }}</p>
+          <button @click="DeleteUser(user.id)">Delete</button>
+      </div>
+    </div>
+
+  </main>
+
+</template>
+
+<style>
+
+</style>
